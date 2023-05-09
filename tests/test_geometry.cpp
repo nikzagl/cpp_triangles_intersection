1865807fcd3b96e6d903a4c791f0f5bec2449449
m.ekstrin <m.ekstrin@tinkoff.ru>
2023-05-09 06:23:43 +0300

    add tests

--- tests/test_geometry.cpp
+++ tests/test_geometry.cpp
@@ -3,7 +3,6 @@
 #include "../include/geometry.hpp"
 
 TEST(test_geometry, is_approximately_equal_true){
-
     Point first_point{1.0, 1.0};
     Point second_point{1.00000001, 1.00000002};
     ASSERT_TRUE(is_approximately_equal(first_point, second_point));
