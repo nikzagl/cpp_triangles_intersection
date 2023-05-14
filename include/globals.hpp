53b59c15acc752defe6e900da36291b9279ae7fb
nikzagl <2ds14k@gmail.com>
2023-05-14 16:12:32 +0300

    Test commit to check CI

--- include/globals.hpp
+++ include/globals.hpp
@@ -2,7 +2,7 @@
 #define CPP_TRIANGLES_INTERSECTION_GLOBALS_HPP
 #include <cmath>
 
-constexpr double max_error = 1e-7;
+constexpr double max_error   = 1e-7;
 constexpr double result_max_error=1e-4;
 constexpr int triangle_points_num = 3;
 #endif
